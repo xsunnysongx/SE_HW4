56b4ea8fbeb76f888a11a0f92787c0a98218ec25
xsunnysongx <92087005+xsunnysongx@users.noreply.github.com>
2024-05-26 15:48:21 +0800

    Add files via upload

---
+++ a.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main()      // main program
+{               // body
+    cout << "Hello world" << endl;   // output statement
+    return 0                     // return statement
+}
